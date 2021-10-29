e933c09278c415a01f2d4f9573b24624862c8015
corykim08 <hyungkkim306@gmail.com>
2021-10-28 21:14:57 -0400

    ml

--- app/src/main/java/corykim/engassistant/MainActivity.java
+++ app/src/main/java/corykim/engassistant/MainActivity.java
@@ -1,24 +1,52 @@
 package corykim.engassistant;
 
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import corykim.engassistant.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    String key = "5deb918661344d0d9c684eeca23bf724";
+    String location = "centralus";
     ActivityMainBinding binding;
     int x = 0;
+    SpeechConfig speechConfig = SpeechConfig.fromSubscription(key, location);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Initialize SpeechSDK and request required permissions.
+        try {
+            // a unique number within the application to allow
+            // correlating permission request responses with the request.
+            int permissionRequestId = 5;
+
+            // Request permissions needed for speech recognition
+            ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO, INTERNET, READ_EXTERNAL_STORAGE}, permissionRequestId);
+        } catch (Exception ex) {
+            Log.e("SpeechSDK", "could not init sdk, " + ex.toString());
+        }
 
         binding.corykim.setText("I am Cory hahaha");
 
@@ -28,7 +56,22 @@ public class MainActivity extends AppCompatActivity {
                 x = x + 1;
                 Toast.makeText(getApplicationContext(),"Button click count = " + x,Toast.LENGTH_SHORT).show();
                 binding.minlee.setText("Button click count = " + x);
+                try {
+                    fromMic(speechConfig);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    public static void fromMic(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+        AudioConfig audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+        SpeechRecognizer recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+        Future<SpeechRecognitionResult> task = recognizer.recognizeOnceAsync();
+        SpeechRecognitionResult result = task.get();
+        Log.e("fromMic", "RECOGNIZED: Text=" + result.getText());
     }
 }
